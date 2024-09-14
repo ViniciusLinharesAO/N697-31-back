ff26bad49900c032957e3ffb9b56e6f99146a8ea
Maurício "Conde" Ramon <mcondero@hotmail.com>
2024-09-14 20:09:54 -0300

    feature: add categories routes at 'index.js'

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const { insertUser, getUsers, updateUser, deleteUser } = require('./database'); // Importa as funções do database.js
+const { insertUser, getUsers, updateUser, deleteUser, insertCategory, getCategories, updateCategory, deleteCategory } = require('./database'); // Inclui as funções de categoria
 
 // Inicializa o app Express
 const app = express();
@@ -17,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Bem-vindo ao servidor com SQLite e Express!');
 });
+
+/* Rotas de Usuários */
 
 // Rota para inserir um novo usuário
 app.post('/users', (req, res) => {
@@ -60,6 +61,56 @@ app.delete('/users/:id', (req, res) => {
   deleteUser(id, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message || 'Erro ao deletar usuário' });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+/* Rotas de Categorias */
+
+// Rota para inserir uma nova categoria
+app.post('/categories', (req, res) => {
+  const { name } = req.body;
+  insertCategory(name, (err, category) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao inserir categoria' });
+    } else {
+      res.status(201).json({ message: 'Categoria inserida com sucesso!', category });
+    }
+  });
+});
+
+// Rota para obter todas as categorias
+app.get('/categories', (req, res) => {
+  getCategories((err, categories) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao buscar categorias' });
+    } else {
+      res.json(categories);
+    }
+  });
+});
+
+// Rota para atualizar uma categoria
+app.put('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  updateCategory(id, name, (err, updatedCategory) => {
+    if (err) {
+      res.status(500).json({ error: err.message || 'Erro ao atualizar categoria' });
+    } else {
+      res.json({ message: 'Categoria atualizada com sucesso!', category: updatedCategory });
+    }
+  });
+});
+
+// Rota para deletar uma categoria
+app.delete('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  deleteCategory(id, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message || 'Erro ao deletar categoria' });
     } else {
       res.json(result);
     }
